f4a605b8e8cb1c041cb88d3a013bbe796444edfa
neener <janinemrosen@gmail.com>
2016-10-12 21:03:40 -0400

    stubbed out working interface with fake data

--- nys_prison/lib/nys_prison/cli.rb
+++ nys_prison/lib/nys_prison/cli.rb
@@ -1,7 +1,39 @@
 class NysPrison::CLI
 
 	def call
+		list_prisons
+		menu
+		goodbye
+	end
+
+	def list_prisons
 		puts "Welcome to the NYS prison directory"
+		puts <<-DOC
+			1. the first prison
+			2. the second prison
+		DOC
+	end
+
+	def menu
+		input = nil
+		while input != "exit"
+			puts "Enter the number of the prison you would like more info or type list to see more prisons or type exit:"
+			input = gets.strip.downcase
+			case input
+			when "1"
+				puts "more info on prison 1..."
+			when "2"
+				puts "more info on prison 2..."
+			when "list"
+				list_prisons
+			else
+				puts "Not sure what you want type list or exit"
+			end
+		end
+	end
+
+	def goodbye
+		puts "See u later gator!"
 	end
 
 end
